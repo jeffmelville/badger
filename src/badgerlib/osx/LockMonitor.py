afe2cd2bfafb2811e15d31d07702e69a5d595a3c
Jeff Melville <dev@jeffmelville.com>
2012-09-15 02:25:51 -0400

    Add basic Mac OS X LockMonitor implementation

--- src/badgerlib/osx/LockMonitor.py
+++ src/badgerlib/osx/LockMonitor.py
@@ -23,9 +23,16 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-class LockMonitor:
-    def __init__(self, dispatcher, parent=None):
+import time
+import threading
+import psutil
 
+
+class LockMonitor:
+
+    LOCK_SCREEN_PROCESS = "ScreenSaverEngine"
+
+    def __init__(self, dispatcher, parent=None):
         self.screen_locked = False
         self.monitoring = False
         self.dispatcher = dispatcher
@@ -35,6 +42,38 @@ class LockMonitor:
     def get_status(self):
         return self.screen_locked
 
-    def monitor(self): pass
+    def monitor(self):
+        self.proc_reader = threading.Thread(target=self.run)
+        self.running = True
+        self.proc_reader.start()
 
-    def shutdown(self): pass
+    def shutdown(self):
+        self.running = False
+
+    def run(self):
+        lock_process = None
+        while self.running:
+            #try to find the process that signals the screen is locked
+            lock_process = self.get_lock_process()
+            #will return None if the screen is not locked
+            #update state, and sleep before checking again
+            #because for the process being created, we can only poll
+            if lock_process is None:
+                self.screen_locked = False
+                self.dispatcher.update_locked(self.screen_locked)
+                time.sleep(0.5)
+            #once the process exists, there is an API to wait for it to complete
+            #signal that the screen is locked, then wait until it ends to go back
+            #through the loop.
+            else:
+                self.screen_locked = True
+                self.dispatcher.update_locked(self.screen_locked)
+                lock_process.wait()
+
+    def get_lock_process(self):
+        result = None
+        for p in psutil.process_iter():
+            if p.name == self.LOCK_SCREEN_PROCESS:
+                result = p
+                break
+        return result
