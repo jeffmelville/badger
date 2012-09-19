17fcd2945c09c54d35db58049e0d7a97d51a56e7
Jeff Melville <dev@jeffmelville.com>
2012-09-18 20:13:51 -0400

    Updated setup.py to support py2app on Mac as well as py2exe on windows

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from distutils.core import setup
-import py2exe
 import sys
 sys.path.append("src")
 
-files = [('', ['src/freakingidiot.wav', 'src/config.json', 'src/badger.png'])]
 
-setup(windows=['src/badger.pyw'],
-    data_files = files,
-    options = {
-    "py2exe": {"dll_excludes": ["MSVCP90.dll"]}
-    })
+files = [('', ['src/freakingidiot.wav', 'src/config.json', 'src/badger.png'])]
+if sys.platform=="win32":
+    import py2exe
+    setup(windows=['src/badger.pyw'],
+        data_files = files,
+        options = {
+        "py2exe": {"dll_excludes": ["MSVCP90.dll"]}
+        })
+elif sys.platform=="darwin":
+    import py2app
+    setup(app=['src/badger.pyw'],
+        data_files = files)
+else:
+    print "Installer not supported on this platform"
+    exit(1)
